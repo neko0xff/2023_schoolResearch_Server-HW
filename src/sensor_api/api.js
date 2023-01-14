f818120ee15433beeb82b2ec43f4207c7d7666f9
zangmenhsu <zangmenhsu@yahoo.co.jp>
2023-01-14 20:22:43 +0800

    20230114 update3

--- src/sensor_api/api.js
+++ src/sensor_api/api.js
@@ -1,6 +1,7 @@
 /*相関函式庫*/
 var express = require('express');
 var bodyParser = require('body-parser');
+const compression = require('compression');
 
 /*時間*/
 var clock=require('./modules/clock.js');
@@ -16,6 +17,7 @@ var server = app.listen(port,function(){
    console.log(clock.consoleTime()+" : API Server is Start!");
    console.log(clock.consoleTime()+" : API Server URL: http://[Server_IP]:%s",port);
 });
+app.use(compression());
 
 /*測試是否運行*/
 app.get('/',function(req,res){
@@ -26,17 +28,23 @@ app.get('/testDB',function(req,res){
     var cnSql='SELECT 1 + 1 AS solution';
     var output;
     console.log(clock.consoleTime()+" : GET /testDB");
-    cnDB=database.cnDB();
-    cnDB.connect();
-    cnDB.query(cnSql, function (error, results, fields) {
+
+    /*run*/
+    try{
+        cnDB=database.cnDB();
+        cnDB.connect();
+        cnDB.query(cnSql, function (error, results, fields) {
+            var dbValue = results[0].solution;
+            var str= "The solution is: " + dbValue.toString();
+            console.log(clock.consoleTime()+" : "+str);
+            res.send(str);
+        });
+    }catch(error){
         if(error)throw error;
-        var dbValue = results[0].solution;
-        var str= "The solution is: " + dbValue.toString();
-        console.log(clock.consoleTime()+" : "+str);
-        res.send(str);
-    });
-    cnDB.end();
-})
+    }finally{
+        cnDB.end();
+    };
+});
 
 /*開發版上傳專用*/
 //api: /upload/:deviceID/data? 
@@ -52,16 +60,21 @@ app.post('/upload/:deviceID/data',async function(req, res){
     var data="("+hum+","+temp+","+tvoc+","+ co2+");";
     var uploadSQL="INSERT INTO "+device_ID+"_Table(hum,temp,tvoc,co2) VALUES"+data;
     
-    cnDB=database.cnDB();
-    cnDB.connect();
-    cnDB.query(uploadSQL, function (error, results, fields) {
-        if(error)throw error;   
+    /*run*/
+    try{
+        cnDB=database.cnDB();
+        cnDB.connect();
+        cnDB.query(uploadSQL, function (error, results, fields) {
         var upload = device_ID +' is Update!';
         console.log(clock.consoleTime()+" : "+ upload);
         res.send(upload);
-        cnDB.end();
     });
-})
+    }catch(error){
+        if(error)throw error;   
+    }finally{
+        cnDB.end();
+    };
+});
 
 /*讀值*/
 // api: /read/:deviceID
@@ -71,30 +84,40 @@ app.get('/read/'+':deviceID'+'/hum',function(req, res) {
     var readSQL='SELECT hum FROM '+ device_ID+'_Table ORDER BY `date` AND `time` DESC LIMIT 1;';
     console.log(clock.consoleTime()+" : GET /read/"+device_ID+"/hum");
     
-    cnDB=database.cnDB();
-    cnDB.connect();
-    cnDB.query(readSQL, function (error, results, fields) {
+    /*run*/
+    try{
+        cnDB=database.cnDB();
+        cnDB.connect();
+        cnDB.query(readSQL, function (error, results, fields) {
+            var output = results;
+            console.log(clock.consoleTime()+" : "+ output);
+            res.send(output); 
+        });
+    }catch(error){
         if(error)throw error;
-        var output = results;
-        console.log(clock.consoleTime()+" : "+ JSON.stringify(output["hum"]));
-        res.send(output);
+    }finally{
         cnDB.end();
-    });
+    };
 });
 app.get('/read/'+':deviceID'+'/temp',function(req, res){
     var device_ID=req.params.deviceID;
     var readSQL='SELECT temp FROM '+ device_ID+'_Table ORDER BY `date` AND `time` DESC LIMIT 1;';
     console.log(clock.consoleTime()+" : GET /read/"+device_ID+"/temp");
     
-    cnDB=database.cnDB();
-    cnDB.connect();
-    cnDB.query(readSQL, function (error, results, fields) {
+    /*run*/
+    try{
+        cnDB=database.cnDB();
+        cnDB.connect();
+        cnDB.query(readSQL, function (error, results, fields) {
+            var output = results;
+            console.log(clock.consoleTime()+" : "+ output);
+            res.send(output); 
+        });
+    }catch(error){
         if(error)throw error;
-        var output = results;
-        console.log(clock.consoleTime()+" : "+ output);
-        res.send(output);
+    }finally{
         cnDB.end();
-    });
+    };
     
 });
 app.get('/read/'+':deviceID'+'/tvoc',function(req, res){
@@ -102,28 +125,38 @@ app.get('/read/'+':deviceID'+'/tvoc',function(req, res){
     var readSQL='SELECT tvoc FROM '+ device_ID+'_Table ORDER BY `date` AND `time` DESC LIMIT 1;';
     console.log(clock.consoleTime()+" : GET /read/"+device_ID+"/tvoc");
     
-    cnDB=database.cnDB();
-    cnDB.connect();
-    cnDB.query(readSQL, function (error, results, fields) {
+    /*run*/
+    try{
+        cnDB=database.cnDB();
+        cnDB.connect();
+        cnDB.query(readSQL, function (error, results, fields) {
+            var output = results;
+            console.log(clock.consoleTime()+" : "+ output);
+            res.send(output); 
+        });
+    }catch(error){
         if(error)throw error;
-        var output = results;
-        console.log(clock.consoleTime()+" : "+ output);
-        res.send(output);
+    }finally{
         cnDB.end();
-    }); 
+    };
 });
 app.get('/read/'+':deviceID'+'/co2',function(req, res){
     var device_ID=req.params.deviceID;
     var readSQL='SELECT co2 FROM '+ device_ID+'_Table ORDER BY `date` AND `time` DESC LIMIT 1;';
     console.log(clock.consoleTime()+" : GET /read/"+device_ID+"/co2");
     
-    cnDB=database.cnDB();
-    cnDB.connect();
-    cnDB.query(readSQL, function (error, results, fields) {
+    /*run*/
+    try{
+        cnDB=database.cnDB();
+        cnDB.connect();
+        cnDB.query(readSQL, function (error, results, fields) {
+            var output = results;
+            console.log(clock.consoleTime()+" : "+ output);
+            res.send(output); 
+        });
+    }catch(error){
         if(error)throw error;
-        var output = results;
-        console.log(clock.consoleTime()+" : "+ output);
-        res.send(output);
+    }finally{
         cnDB.end();
-    });  
+    };
 });
