5bc63fe7297d25838f57435db7e504749c8a8242
neko0xff <zangmenhsu@yahoo.co.jp>
2023-08-16 14:12:18 +0800

    20230816 update 2
    add RateLimit

--- src/Server/IoTGateway/modules/httpServer.js
+++ src/Server/IoTGateway/modules/httpServer.js
@@ -1,10 +1,17 @@
 var express = require("express");
+var RateLimit = require('express-rate-limit');
 var swaggerUi = require("swagger-ui-express");
 var helmet = require("helmet");
 const compression = require("compression");
 var ConfigParser = require("configparser");
 const swaggerDocument = require("./config/swagger.json");
 var clock=require("./clock.js");
+
+/*限制使用者存取的時間*/
+var limiter = RateLimit({
+    windowMs: 1*60*1000, // 1 minutes
+    max: 100, // limit each IP to 100 requests per window Ms
+});
 
 /*Server 起始設定*/
 const configSet = new ConfigParser();
@@ -14,6 +21,7 @@ var httpServer=express();
 var port=configSet.get("Service","HTTP"); 
 
 /*啟用時使用的設定*/
+httpServer.use(limiter); //啟用限流
 httpServer.use(compression()); //啟用gzip壓縮
 httpServer.use(express.urlencoded({ extended: false })); //傳送方式：x-www-form-urlencoded
 httpServer.use(express.json()); //傳送方式：json
