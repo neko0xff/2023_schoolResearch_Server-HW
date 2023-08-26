fb50d60b6db6334e10e3c62526917af06c5a4121
neko0xff <zangmenhsu@yahoo.co.jp>
2023-08-27 07:43:19 +0800

    20230827 update

--- src/Server/IoTGateway/firewall.sh
+++ src/Server/IoTGateway/firewall.sh
@@ -1,6 +1,3 @@
-echo "Create docker Container"
-docker compose up --build -d
-
 echo "Add Firewall rule(Firewalld)"
 firewall-cmd --add-port=3095/tcp --permanent
 firewall-cmd --add-port=3094/tcp --permanent
