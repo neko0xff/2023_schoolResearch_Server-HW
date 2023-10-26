ae7ce5b21bb2166f111fa17484abdd8b1b3f3cf3
neko0xff <zangmenhsu@yahoo.co.jp>
2023-10-26 16:22:34 +0800

    20231026 update2

--- src/Server/serverConfig/docker/Dashboard/run-v2.sh
+++
@@ -1,9 +0,0 @@
-echo "Running a Service Dashboard"
-docker compose up -d
-echo "Add Firewall rules"
-firewall-cmd --add-port=8000/tcp --permanent
-firewall-cmd --add-port=8083/tcp --permanent
-firewall-cmd --add-port=8084/tcp --permanent
-firewall-cmd --add-port=9000/tcp --permanent
-firewall-cmd --reload
-echo "Finnish Create Service Dashboard"
