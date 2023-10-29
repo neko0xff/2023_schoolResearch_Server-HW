370693a3446ec06dab2fd36765689e630b49f529
neko0xff <zangmenhsu@yahoo.co.jp>
2023-10-29 13:18:20 +0800

    20231019 update2

--- config/server/docker/testCmd/grafana.sh
+++
@@ -1,5 +0,0 @@
-port1="8085:3000"
-volume1="/var/run/docker.sock:/var/run/docker.sock"
-image="grafana/grafana-oss"
-ContainerName="grafana-oss"
-docker run -d -v $volume1 -p $port1 --name=$ContainerName $image
--- config/server/docker/testCmd/mqttExplorer.sh
+++
@@ -1,6 +0,0 @@
-echo "Set Container"
-docker run -d -p 9515:9515 --name=mqttExplorer --restart=always -v /var/run/docker.sock:/var/run/docker.sock ccll/mqtt-explorer
-echo "Set Firewall"
-firewall-cmd --add-port=9515/tcp --permanent
-firewall-cmd --add-port=9515/udp --permanent
-firewall-cmd --reload
