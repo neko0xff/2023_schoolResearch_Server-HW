b24a4428f9c1d1ff46d53d3bc02e9ff0fe89da41
neko0xff <neko_0xff@protonmail.com>
2024-04-15 07:17:19 +0800

    20240415 update

---
+++ src/Arduino/prototype/AVR/240414-070951-nanoatmega328/src/fan_pwm.ino
@@ -0,0 +1,52 @@
+/*
+* board: Arduino Nano(AT Mega 328p)
+* 4pin接法: 
+* 1. pwm部分: 接風扇的pin4
+* 2. 風扇的正極: 請接5V以上的供電源
+*/
+
+#include <Arduino.h>
+
+/*腳位設置*/
+int fan_pin = 9;          // 風扇: D9
+int switch_pin = A0;      // 開關: A0
+
+/*計算風扇速度的pwm輸出*/
+int fan_speed(int pot){
+  int  cal = pot * (255 / 1023.0); // 計算輸出部分: 介於 0-255
+  return cal;
+}
+
+/*在序列監控視窗中輸出的值*/
+void serialOuput_task01(int num){
+  //開關值
+  Serial.print("Switch=");
+  Serial.print(num);
+  Serial.print(",");  
+  delay(1000); // delay 1 sec
+}
+void serialOuput_task02(int num){
+  //風扇速度
+  Serial.print("Speed=");
+  Serial.print(num);
+  Serial.println("");
+  //Serial.println(","); 
+  delay(1000); // delay 1 sec
+}
+
+/*主程式*/
+void setup() {
+ Serial.begin(9600);                   // 初始化傳輸速率: 9600 bps
+ pinMode(fan_pin, OUTPUT);             // 指定fan腳位: 輸出
+ pinMode(switch_pin,INPUT);            // 指定開關腳位: 輸入
+}
+
+void loop() {
+ int pot = analogRead(switch_pin);     // 讀取類比訊號
+ int speed_cal = fan_speed(pot);
+ 
+ /*輸出*/
+ analogWrite(fan_pin,speed_cal);           // pwm訊號
+ serialOuput_task01(pot); 
+ serialOuput_task02(speed_cal); 
+}
