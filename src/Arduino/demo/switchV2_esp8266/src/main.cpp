ae2c22be1fb660a008f0f31c2759443b7045eb1b
neko0xff <neko_0xff@protonmail.com>
2024-04-25 05:17:38 +0800

    20240424 update2

--- src/Arduino/demo/switchV2_esp8266/src/main.cpp
+++ src/Arduino/demo/switchV2_esp8266/src/main.cpp
@@ -11,6 +11,7 @@
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
 #include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+#include <LedControl.h> 
 
 /*連線部分*/
 #include "espCN.h"
@@ -19,6 +20,12 @@ const char* deviceName_char = espcn.deviceName.c_str();
 WiFiServer server(80); // AP URL: http://192.168.4.1:80
 WiFiManager wifiManager;
 boolean pirVal = false;   //目前重置電位狀態
+
+/* LED OUT */
+byte smile[8] = {0x3C,0x42,0xA5,0x81,0xA5,0x99,0x42,0x3C};  // 微笑
+byte love[8]  = {0x00,0x66,0x99,0x81,0x42,0x24,0x18,0x00};   // 愛心
+byte m[8]=     {0xE7,0xFF,0xFF,0xDB,0xDB,0xDB,0xC3,0xC3};  //英文字母m
+byte i[8]=     {0x00,0x10,0x00,0x10,0x10,0x10,0x10,0x10};  //英文字母i
 
 /*timer*/
 unsigned long lastTime = 0;
@@ -32,6 +39,48 @@ int fan_off = 0;
 int fan1_pin = D4;          // 風扇1: D4
 int fan2_pin = D5;          // 風扇2: D5
 int reset_button_pin = D6;  // 重置按鈕: D6
+int DIN = D1; // MAX7219_DIN: D1
+int CS =  D2; // MAX7219_CS:  D2
+int CLK = D3; // MAX7219_CLK: D3
+
+
+/*MAX7219 LED Ctr*/
+LedControl lc=LedControl(DIN,CLK,CS,0);
+void MAX7219_ctr(){
+  lc.shutdown(0,false);       //The MAX72XX is in power-saving mode on startup
+  lc.setIntensity(0,15);      // Set the brightness to maximum value
+  lc.clearDisplay(0);         // and clear the display
+}
+void printByte(byte character []){
+  int i = 0;
+  for(i=0;i<8;i++){
+    lc.setRow(0,i,character[i]);
+  }
+}
+void LED_fan1ON(){
+  printByte(smile);
+  delay(1000);
+  lc.clearDisplay(0);
+  delay(1000);
+}
+void LED_fan1off(){
+  printByte(love);    
+  delay(1000);
+  lc.clearDisplay(0);
+  delay(1000);
+}
+void LED_fan2ON(){
+  printByte(i);
+  delay(1000);
+  lc.clearDisplay(0);
+  delay(1000);
+}
+void LED_fan2off(){
+  printByte(m);    
+  delay(1000);
+  lc.clearDisplay(0);
+  delay(1000);
+}
 
 /* WIFI AP 設定頁面 */
 void WebProtalSetup(){
@@ -124,8 +173,18 @@ void httpGet(){
           // Adjust fan speed based on status
           if (name == "fan1") {
             analogWrite(fan1_pin, status ? fan_on : fan_off);
+            if(status == true){
+              LED_fan1ON();
+            }else if(status == false){
+              LED_fan1off();
+            }
           } else if (name == "fan2") {
             analogWrite(fan2_pin, status ? fan_on : fan_off);
+            if(status == true){
+              LED_fan2ON();
+            }else if(status == false){
+              LED_fan2off();
+            }
           }
           
         }
@@ -157,6 +216,7 @@ void setup() {
   pinMode(fan1_pin, OUTPUT);                  // 指定風扇1腳位: 輸出
   pinMode(fan2_pin, OUTPUT);                  // 指定風扇2腳位: 輸出
   pinMode(reset_button_pin, INPUT_PULLUP);    // 指定重置按鈕: 輸入+啟用上拉電阻
+  MAX7219_ctr();
   WebProtalSetup();
 }
 
